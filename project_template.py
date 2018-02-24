88be38206ee6c84a11edb47902d8341921add00b
ErnestoHoro <discusmeniscus@gmail.com>
2018-02-24 02:02:20 +0100

    Added inheritance.py Added regex.py

--- project_template.py
+++ project_template.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-"""
-project_template.py:    A template for python (3) projects.
+"""project_template.py:    A template for python (3) projects.
 
 The Zen of Python, by Tim Peters:
 
@@ -24,22 +23,49 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
+
+Personal Zen Additions, by ErnestoHoro (git):
+
+Follow the code style of the given project - if anyhow reasonable.
+Use comment section blocks with a length of 120 chars to structure blocks of code.
+Wrap lines at 120 chars of the current block with a total maximum of 160 chars.
+Without indentation wrap lines at 120 chars.
+Use linebreaks only for major logical or complex blocks.
+At indentation steps extra linebreaks are omitted.
+If explicit is too ugly, implicit is better.
+If flat is too ugly, nested is better.
+Avoid other styles like "import <lib>".
+Use single quotes by default, except for docstrings because there they might represent actual spoken language.
+Stick to an OOP style.
+Avoid any more complexity than basic instantiation, argument parsing, logging- or gui-initialization at "if __name__ ==
+'__main__':".
+Don't put another useless main().
+Always use the r (raw) prefix for file paths under windows, instead of manual escape.
+Use os.linesep if appropriate.
+Use regex, if appropriate, instead of stacked (e.g.) replace or strip.
+Use braces at ('').join(list).
+Put #!/usr/bin/env python on top.
+Name your files *.py independent of your os-environment.
+Encode your application files (!) by default in UTF-8.
+Use x.y.z (major, minor, build) scheme for versioning.
+Explain the basics of individual code formatting in the 1st docstring section.
 """
 
-__author__ = "Pat Smith"
-__copyright__ = "Copyright 2099, ProjectName"
-__credits__ = ["Sir Toby", "Anton Placeholder"]
-__license__ = "GPL - Put in ./LICENSE directory."
-__version__ = "1.0.1"
-__maintainer__ = "Pat Smith"
-__email__ = "pat.smith@rando.org"
-__status__ = "Production"
+__author__ = 'Pat Smith'
+__copyright__ = 'Copyright 2099, ProjectName'
+__credits__ = ['Sir Toby', 'Anton Placeholder']
+__license__ = 'GPL - Put in ./LICENSE directory.'
+__version__ = '1.0.1'
+__maintainer__ = 'Pat Smith'
+__email__ = 'pat.smith@rando.org'
+__status__ = 'Production'
 
+import os.path
 import sys
 
 
 class ExampleClass:
-    """
+    """This line should be used already.
     This is a Section Title
     =======================
 
@@ -51,6 +77,7 @@ class ExampleClass:
     def __init__(self):
         """OneLine DocString."""
         self.hello_world = 'Hello World!'
+        self.cwd = os.path.getctime(r'C:\Windows\system32.dll')
 
     def print_hello(self):
         """OneLine DocString."""
@@ -63,8 +90,7 @@ class ExampleClass:
 
 
 if __name__ == '__main__':
+    example = ExampleClass()  # inline comments, placed after two spaces, are not real sentences (just hints)
+    example.print_hello()  # Todo: This should be a real sentence, since it is a work instruction!
 
-    example = ExampleClass()
-    example.print_hello()
-
-    ExampleClass.exit()  # calling a staticmethod
+    ExampleClass.exit()
